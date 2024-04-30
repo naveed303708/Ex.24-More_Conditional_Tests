c544cfbdfc33f1fb992b50732a831e76429275de
Naveed Ahmed Farooqui <162836180+naveed303708@users.noreply.github.com>
2024-05-01 01:06:49 +0500

    Add files via upload

---
+++ more_conditional_tests.js
@@ -0,0 +1,28 @@
+"use strict";
+// Equality and Inequality Test 1
+console.log("Equality test with strings: ", "Apple" === "Apple");
+// Equality and Inequality Test 2
+console.log("Inequality test with strings: ", "Apple" != "orange");
+// Test using the lower case function
+console.log("Lower Case function test: ", "HELLO".toLowerCase() === "hello");
+// Numerical tests involving equality and inequality
+console.log("Equality test with numbers: ", 26 === 26);
+// Numerical tests involving equality and inequality
+console.log("Inequality test with numbers: ", 26 != 35);
+//greater than test
+console.log("Greater than test: ", 10 > 5);
+// less than and less than
+console.log("Greater than test: ", 10 > 5);
+//greater than or equal to
+console.log("Greater than and equal to test: ", 10 >= 10);
+//less than or equal to
+console.log("less than or equal to test: ", 5 <= 10);
+//Tests using "and" operator
+console.log("And operator test:, ", 5 === 5 && 10 > 5);
+//Tests using "or" operator
+console.log("or operator test: ", 5 === 5 || false);
+//Test whether an item is in a array
+const fruits = ['Nashpati', 'banana', 'Amrood'];
+console.log('Test "Nashpati" in the array: ', fruits.includes("Nashpati"));
+//Test whether an item is not in a array
+console.log('Testing "Apple" is not in array: ', !fruits.includes('Apple'));
